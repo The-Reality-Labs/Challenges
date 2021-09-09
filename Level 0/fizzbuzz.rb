fdf1e45a8a5c92f6e4de72182492ca7519bcce05
Daniel Gomez-Sierra <daniel.gomez@fair.com>
2021-09-08 20:37:29 -0500

    Fizz Buzz

--- Level 0/fizzbuzz.rb
+++ Level 0/fizzbuzz.rb
@@ -2,3 +2,18 @@
 # For each multiple of 3, print "Fizz" instead of the number.
 # For each multiple of 5, print "Buzz" instead of the number.
 # For numbers which are multiples of both 3 and 5, print "FizzBuzz" instead of the number.
+
+(1..100).to_a.each do |number|
+  res =
+    if number % 3 == 0 && number % 5 == 0
+      "FizzBuzz"
+    elsif (number % 5).zero?
+      "Buzz"
+    elsif number % 3 == 0
+      "Fizz"
+    else
+      number
+    end
+
+  puts res
+end
